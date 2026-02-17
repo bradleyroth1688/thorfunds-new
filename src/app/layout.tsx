3b4d02aa9bc3842a1a93dfeeb22364dee327f95a
Bradley Roth <bradleyroth@Bradleys-MacBook-Pro.local>
2026-02-17 15:37:23 -0500

    Add LinkedIn Insight Tag for retargeting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,6 +116,25 @@ export default function RootLayout({
         <meta name="theme-color" content="#1a365d" />
         {/* Organization Schema - Site Wide */}
         <SchemaScript schema={organizationSchema} />
+        {/* LinkedIn Insight Tag */}
+        <Script id="linkedin-insight" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "86s9628nsfk0o3";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);})(window.lintrk);
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display: "none"}} alt="" src="https://px.ads.linkedin.com/collect/?pid=86s9628nsfk0o3&fmt=gif" />
+        </noscript>
       </head>
       <body className={`${inter.className} antialiased bg-white text-navy-800`}>
         {/* Skip to main content for accessibility */}
